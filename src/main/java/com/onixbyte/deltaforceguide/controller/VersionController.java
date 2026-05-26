0d70b27653a420ac722d13edf06fab10f9a21abe
siujamo <jamo.siu@gmail.com>
2026-05-26 14:24:56 +0800

    feat: add OpenAPI definition with title, contact, and licence

--- src/main/java/com/onixbyte/deltaforceguide/controller/VersionController.java
+++ src/main/java/com/onixbyte/deltaforceguide/controller/VersionController.java
@@ -2,10 +2,12 @@ package com.onixbyte.deltaforceguide.controller;
 
 import com.onixbyte.deltaforceguide.service.AppService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "版本信息")
 @RestController
 @RequestMapping("/versions")
 public class VersionController {
