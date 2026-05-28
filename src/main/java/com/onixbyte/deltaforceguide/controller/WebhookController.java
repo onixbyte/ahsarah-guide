8d24b6082da8012ebe1589ad9b2789338a5678d8
siujamo <jamo.siu@gmail.com>
2026-05-28 15:18:25 +0800

    feat: add gitlab webhook http entrypoint

---
+++ src/main/java/com/onixbyte/deltaforceguide/controller/WebhookController.java
@@ -0,0 +1,20 @@
+package com.onixbyte.deltaforceguide.controller;
+
+import com.onixbyte.deltaforceguide.domain.dto.GitLabWebhookRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/webhook")
+public class WebhookController {
+
+    private static final Logger log = LoggerFactory.getLogger(WebhookController.class);
+
+    @PostMapping("/gitlab")
+    public void gitlabWebhook(
+            @RequestBody GitLabWebhookRequest request
+    ) {
+        log.info("request={}", request);
+    }
+}
