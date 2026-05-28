9bc70d5370ad5a47b8ddd03ebd69be427fc116be
siujamo <jamo.siu@gmail.com>
2026-05-28 15:17:36 +0800

    feat: add web traffic logger

--- src/main/java/com/onixbyte/deltaforceguide/config/AppConfig.java
+++ src/main/java/com/onixbyte/deltaforceguide/config/AppConfig.java
@@ -1,10 +1,26 @@
 package com.onixbyte.deltaforceguide.config;
 
+import com.onixbyte.deltaforceguide.interceptor.TrafficInterceptor;
 import com.onixbyte.deltaforceguide.properties.AppProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    private final TrafficInterceptor trafficInterceptor;
+
+    @Autowired
+    public AppConfig(TrafficInterceptor trafficInterceptor) {
+        this.trafficInterceptor = trafficInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(trafficInterceptor);
+    }
 }
